ad5bf139e633ff2ebdcf961f54e562d5613c3b25
Elite1dev1 <ikeohachiedozie2020@gmail.com>
2025-02-28 02:16:06 +0100

    trying to fix build

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -5,11 +5,13 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
+interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorColor?: string;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentProps<typeof ProgressPrimitive.Root> & {
-    indicatorColor?: string;
-  }
+  ProgressProps
 >(({ className, value, indicatorColor, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
@@ -22,7 +24,7 @@ const Progress = React.forwardRef<
     <ProgressPrimitive.Indicator
       className={cn(
         "h-full w-full flex-1 transition-all",
-        indicatorColor
+        indicatorColor || "bg-primary"
       )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
